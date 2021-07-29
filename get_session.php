817e6f7339092dafcb62766cfe25730988b6e488
Brayan Angarita <brayanangarita11@gmail.com>
2021-07-29 17:09:48 -0500

    GetSession file changed

--- get_session.php
+++ get_session.php
@@ -1,6 +1,40 @@
 <?php
-use \Tsugi\Util\U;
+require_once "../config.php";
 
-$session = U::addSession('student.php');
-var_dump("New session set to user student");
-echo $session;
+// The Tsugi PHP API Documentation is available at:
+// http://do1.dr-chuck.com/tsugi/phpdoc/namespaces/Tsugi.html
+
+use \Tsugi\Util\U;
+use \Tsugi\Core\Settings;
+use \Tsugi\UI\SettingsForm;
+use \Tsugi\Core\LTIX;
+use \Tsugi\Util\Net;
+
+// No parameter means we require CONTEXT, USER, and LINK
+$LAUNCH = LTIX::requireData(); 
+
+// Model
+$old_code = Settings::linkGet('code', '');
+$retval = new \stdClass();
+$retval->status = "failure";
+$retval->detail = "";
+Output::jsonOutput($retval);
+
+// Render view
+$OUTPUT->header();
+$OUTPUT->bodyStart();
+$OUTPUT->topNav();
+
+$OUTPUT->footerStart();
+
+?>
+<script>
+$(document).ready(function(){
+    console.log("We are ready to send data");
+    var url = '<?= U::addSession('api/attend.php') ?>';
+    console.log("The URL is: " + url);
+});
+</script>
+<?php
+$OUTPUT->footerEnd();
+
