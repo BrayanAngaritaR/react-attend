e15fd7a0a976cd3ab2030db3d5f6db3d9c1e1eee
Brayan Angarita <brayanangarita11@gmail.com>
2021-07-29 09:27:35 -0500

    Headers added to api.php file

--- api/attend.php
+++ api/attend.php
@@ -2,6 +2,8 @@
 require_once "../../config.php";
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: content-type");
+header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE");
 
 use \Tsugi\Core\LTIX;
 use \Tsugi\UI\Output;
