0838265ef0c62d09f1050c252c73bfb541109151
Brayan Angarita <brayanangarita11@gmail.com>
2021-08-03 20:21:19 -0500

    New CORS added

--- api/index.php
+++ api/index.php
@@ -1,10 +1,19 @@
 <?php
 
-$domain = "http://localhost:3000/";
+// $domain = "http://localhost:3000/";
 
-header("Access-Control-Allow-Origin: $domain");
-header("Access-Control-Allow-Headers: content-type");
-header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE");
+// header("Access-Control-Allow-Origin: $domain");
+// header("Access-Control-Allow-Headers: content-type");
+// header("Access-Control-Allow-Methods: OPTIONS,GET,PUT,POST,DELETE");
+
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 86400');    // cache for 1 day
+
+
 print_r("Hello");
 
 ?>
+
+"Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
